d7f325e7bfb67784f0805737af598dfce7f671b8
saasbook <saasbook@saasbook.(none)>
2013-11-25 09:43:03 -0600

    refactored bugfix

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,11 +25,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    if !params[:id]
-      @category = Category.new
-    else
-      @category = Category.find(params[:id])
-    end
+    @category = params[:id] ? Category.find(params[:id]) : Category.new
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
